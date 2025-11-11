852c84943680c0e7e7cd35a501d28f19e039a174
Niklas Haiden <niklas.haiden@gmail.com>
2025-11-11 20:10:59 +0100

    feat(blog): enable aurora blog feature and write first blog post

---
+++ blog/2025-11-11-hello-aurora-blog.md
@@ -0,0 +1,15 @@
+---
+title: Welcome to the Aurora Blog
+description: The first post on this blog!
+slug: hello-aurora-blog
+authors: niklas
+---
+
+Hello stargazers, welcome to this first post on the Aurora Blog!! Exciting things to follow.
+<!-- truncate -->
+We finally enabled the Blog Feature on Docusaurus and now we can better inform you all about our new things that happen in Aurora, so exciting!
+
+Lots more to come!
+
+Happy stargazing
+~ Niklas
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,17 @@ const config: Config = {
         theme: {
           customCss: "./src/css/custom.css",
         },
+        blog: {
+        showReadingTime: true,
+        feedOptions: {
+          type: ['rss', 'atom'],
+          xslt: true,
+        },
+        editUrl: 'https://github.com/ublue-os/aurora-docs/tree/main/',
+        // Recommended: Remove onInlineTags/onInlineAuthors if present
+      },
       } satisfies Preset.Options,
+
     ],
   ],
 
@@ -104,6 +114,9 @@ const config: Config = {
           label: "GitHub",
           position: "right",
         },
+        {
+          to: 'blog', label: 'Blog', position: 'left', // or position: 'right'
+        }
       ],
     },
     footer: {
